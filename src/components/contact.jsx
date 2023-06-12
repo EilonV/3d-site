02c0c9f91bc5706e314ddb731b6c8c658fd79688
EilonV <73593619+EilonV@users.noreply.github.com>
2023-06-12 14:53:44 +0300

    alt, metadata

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,16 +6,16 @@ import linkedin from '../assets/pics/linkedin.svg'
 export const Contact = () => {
     return <footer className="contact">
         <a href="https://www.github.com/eilonv" target='_blank' rel="noreferrer">
-            <img src={git} alt="" />
+            <img src={git} alt="github link icon" />
         </a>
         <a href="https://www.facebook.com/dontmindmejustcleaninguphere" target='_blank' rel="noreferrer">
-            <img src={facebook} alt="" />
+            <img src={facebook} alt="facebook link icon" />
         </a>
         <a href="https://www.instagram.com/eilon_vana" target='_blank' rel="noreferrer">
-            <img src={instagram} alt="" />
+            <img src={instagram} alt="instagram link icon" />
         </a>
         <a href="https://www.linkedin.com/in/eilonvana" target='_blank' rel="noreferrer">
-            <img src={linkedin} alt="" />
+            <img src={linkedin} alt="linkedin link icon" />
         </a>
     </footer>
 }
